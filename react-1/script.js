154ebb124719b53d4794f828609d306994ccc290
kimjunseok777 <wnszn5183@naver.com>
2024-06-07 21:45:37 +0900

    react

--- react-1/script.js
+++ react-1/script.js
@@ -8,7 +8,7 @@ const $form = document.querySelector('.form')
     폼 안에 입력 값을 action에 전달하는 역할
 */
 
-// submit 이벤트를 넣게 되면, 이벤트에 form event 라는 친구가 전달이 된다
+// submit 이벤트를 넣게 되면, 이벤트에 formEvent 라는 친구가 전달이 된다
 $form.addEventListener('submit', (event) => {
     event.preventDefault()
     // form은 action 으로 주소를 이동해 데이터를 보내는 기능을 가지고 있는데,
@@ -16,12 +16,14 @@ $form.addEventListener('submit', (event) => {
 
     console.log(event.target.email.value)
     // name 이라는 값을 통해서 target 에 가지고 있는 value 를 가지고 오는 것
+    //--> name="email"  -->  이렇게 속성으로 name 값을 지정해줬다
     // 즉, form 안에 있는 input 의 value 를 가지고 올 수 있는 것이다
 
     /*
         submit을 통한 formEvent에 input에 주어진 name을 활용하면
         value를 가지고 올 수 있다
         --> 위에 $emailInput 처럼 일일히 지정해줄 필요 없다
+        --> 즉, DOM API 에 접근할 필요가 없어지는 것이다
     */
 
     /*
@@ -34,7 +36,7 @@ $form.addEventListener('submit', (event) => {
     form 은 "제출(submit)" 이 되면, 다른 웹사이트 action 으로 정해진 페이지로
     이동시키려는 습관이 있다
 
-    -->  이런 습관 (기존에 있는 이벤트) 를 막는 기능을 사용해야한다
+    -->  이런 습관 (기존에 있는 이벤트) 을 막는 기능을 사용해야한다
     -->  preventDefault() 사용해야 한다
     -->  event.preventDefault()
 
@@ -53,7 +55,7 @@ $form.addEventListener('submit', (event) => {
             input 에는 "패턴" 아라는 값이 있는데, input의 값이 이 패턴에서 어긋나면
             자바스크립트에서 이걸 감지하는 기능이 있다
 
-            ex : 이거 이메일 양식이 아니야, 이거 숫자가 아니야 ... 등등 이런식으로 유효성 검사
+            ex) 이거 이메일 양식이 아니야, 이거 숫자가 아니야 ... 등등 이런식으로 유효성 검사
 
             ==> 나중에 이런 유효성 검사를 해주는 라이브러리를 사용할
 
@@ -62,7 +64,7 @@ $form.addEventListener('submit', (event) => {
 
 
     --> form 태그는 원래는 action으로 데이터를 보내기 위해서 사용했던 것이다
-            하지만, 요즘에는 javaScripot 에서 fetch 나 axios 같은 편의성을 가진
+            하지만, 요즘에는 JavaScript 에서 fetch 나 axios 같은 편의성을 가진
             라이브러리 등이 등장했다  -->  기존의 기능 사용 x
 
             -->  이제 form 태그는 편의성 때문에 기능 때문에 사용하는 것이다
