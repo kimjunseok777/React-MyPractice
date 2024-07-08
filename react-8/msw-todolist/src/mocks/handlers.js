c34069fa61d20639c5ef60ebf6acdd4c240de460
kimjunseok777 <wnszn5183@naver.com>
2024-07-08 21:33:49 +0900

    react

--- react-8/msw-todolist/src/mocks/handlers.js
+++ react-8/msw-todolist/src/mocks/handlers.js
@@ -1,10 +1,10 @@
-import { HttpResponse, http } from "msw"
+import * as UserApi from "./apis/user.api"  //-->  Mock 데이터가 있는 login 받아온 것이다
+// 이렇게 import 받으면 객체형태로 export 되어 있는 것들을 다 가져온다
+// 그렇기에 { login : function } 이렇게 사용할 것이다  -->  export 된 login 으로 value 인 function 만 가져올 것이다
 
-// 이 배열 안에 가상의 api 들을 넣을 것이다
-export const handlers = [
-    http.get("/todo", () => {
-        return HttpResponse.json({
-            id: 1,
-        })
-    })
-]
+
+// 작성하기 전에 msw 공식문서 확인해보자 **  -->  모두 삭제하고 UserApi 로 바꿔줬다
+// 이 handlers 배열 안에 가상의 api 들을 넣을 것이다
+// export 되어 있는 login 의 value 인 function 만 handlers 배열에 넣어준 것이다 (객체의 값만 가져온 것이다)
+export const handlers = [...Object.values(UserApi)]
+//-->  이렇게 하면 handlers 에 자동으로 등록 된 것이기에 사용하면 된다
--- react-8/msw-todolist/src/pages/main/_components/sign-in-form.jsx
+++ react-8/msw-todolist/src/pages/main/_components/sign-in-form.jsx
@@ -27,12 +27,74 @@ const SignInForm = () => {
         resolver: yupResolver(signInFormSchema),
     })
 
-    const handlePressSignIn = (data) => {
-        if(data.email === "test@test.com" && data.password === "testtest") {
-            return navigate("/todo/3")
+    //----------------------------------------------------------------------------------------------
+    // msw 수업 :
+    const handlePressSignIn = async (data) => {
+
+        try{ //==> 에러 핸들링 하기 위해 try , catch 사용해준 것이다
+            //--------------------------------------------------------------------------------------------
+            // 삭제한 부분 :
+            // if(data.email === "test@test.com" && data.password === "testtest") {  -->  실제로 백엔드에게 요청하는 요직으로 바꿀 것이다
+            //     return navigate("/todo/3")
+            // }
+            //--------------------------------------------------------------------------------------------
+
+            // fetch 에 주소 적을 때는 "백엔드의 로그인 하는 주소" 적으면 된다  -->  백엔드가 설정해서 프론트에게 알려준 주소이다 (지금은 가짜로 만들어놓은 것이다)
+            // "/api/user/login"  -->  이 주소에 데이터를 요청하는 것이다
+            const response = await fetch("/api/user/login", {
+                //-->  fetch 는 자바스크리븥에서 내장된 "빌트인객체" 이다 (Math.random 같은 것처럼 아예 자바스크립트에서 내장된 기능)
+                //-->  "/api/user/login" 는 백엔드 주소이다 (우리가 데이터를 보내는 목적지이다  -->  아래 있는 "post" 라는 메소드로 body 데이터를 보내는 것이다)
+                /*
+                    fetch :
+                        -->  fetch 는 데이터를 응답받으면 json 형태로 오지 않고, Promise 형태의 "body" 라고 하는 응답데이터 형태로 온다
+                        -->  Promise 는 아직 진행이 되기 전 상태이며 "비동기" 이기 때문에 await 을 걸거나, then 을 통해서 비동기 처리를 해줘야 한다
+                        -->  fetch 로 받아온 데이터의 형태는 json 이 아니라 string 즉, 문자열 이기 때문에 json 으로 바꿔줘야한다
+
+                    await :
+                        -->  await 은 비동기에서 "기다려" 라고 명령을 내린 부분이다
+                        -->  fetch 는 비동기이기 때문에, await 을 걸지 않으면 밑에 있는 alert 가 먼저 실행이 돼버린다  -->  심지어 alert 의 확인을 누르면 코드들을 다시 실행하지조차 않는다
+                        -->  그렇기에 await 을 걸어서 이 코드를 "동기" 적으로 만들어준 것이다
+                        -->  await 은 자바스크립트에서 비동기 함수 (async function) 내에서 사용되는 키워드로, 프로미스(Promise)가 해결될 때까지 함수의 실행을 중지하고, 프로미스가
+                                해결되면 결과 값을 반환하다  -->  이는 비동기 코드를 "동기식" 코드처럼 읽고 작성할 수 있게 도와준다
+                        -->  await는 오직 async function (비동기 함수) 내부에서만 사용할 수 있다  -->  일반 함수에서는 사용할 수 없습니다
+                        -->  await 은 응답이 올 때까지 기다리는 것이다
+
+                    method :
+                        같은 이름 ("/api/user/login") 이어도 "재사용" 할 수 있게 해주는 친구이다  -->  post(등록) , get(조회) , push(수정) , patch(수정) , delete(삭제)
+                        -->  이렇게 메소드에 따라 같은 주소 ("/api/user/login") 여도 다른 기능을 할 수 있도록 만들어 놓은 것이다
+                        -->  이 메소드를 정하는 주체는 "백엔드" 개발자이다
+
+                    body :
+                        post 나 patch 등의 요청 등의 내부에 데이터를 포장해서  -->  즉, 데이터를 안에 넣어서 보내게 되는데, 이 요청이 (body) https 통신에서는 "암호화" 가 된다
+                        -->  body 안에 있는 내용은 "암호화" 되어서 전달되기 때문에 해커들이 유저들의 데이터를 찾으려고 해도 절대 찾을 수 없다
+                        -->  보안적으로 중요한 경우가 있거나, 데이터를 객체 형태로 보내야될 때는 이렇게 body 에 있는 데이터로 보내면 된다
+                */
+
+                // 이 안에는 전송할 데이터를 적으면 된다 (email 과 password 가 필요하다는 사실도 백엔드가 알려주는 것이다)
+                // "/api/user/login" 이란 백엔드 주소에 아래에 있는 body 데이터를 보내는 것이다
+                method: "post", //-->  method 를 적지 않으면 기본값이 "get" 이기 때문에 "post" 라고 명시해줘야한다  -->  앞에 user.api 에서 post 로 데이터 받는 걸로 설정해놨다
+                body: JSON.stringify({
+                    email: data.email,
+                    password: data.password
+                })
+            }) // .then((res) => res.json())
+            //-->  이렇게 적으면 받아온 데이터를 json 으로 바꾸는 것 까지 상단에 적어놓은 await 이 적용되기에, 아래 response_data 처럼 await 한번 더 안 적어도 된다
+            //-->  굳이 이렇게 안하고, 아래 response_data 처럼 await 걸어 놓고 사용해도 된다
+            //-->  Promise 형태로 데이터가 오기 때문에, await 을 걸어줘야한다
+
+            // fetch 에서 받아온 데이터는 string 형태로 오기 때문에, json 으로 바꿔줘야한다
+            // then 안써준다면 아래처럼 바꿔주면 된다  -->  이렇게 해줘도 된다 (이렇게 await 두번 걸어도 상관 없다)
+            const response_data = await response.json()
+            if(response_data.status === 200) {
+                navigate("/todo/3")  //-->  성공했다면 투두페이지로 이동하는 것이다
+            }
+        } catch(err) { //-->  에러가 나면 여기 catch 가 잡아줘서 "alert(err.error)" 형태로 에러를 보여주는 것이다 (에러를 중간에서 가로채는 것이다)
+            alert(err.error) //-->  에러메세지 잘 안떠서 일단 주석처리 해놓은 것
+            // alert("아이디와 비밀번호를 확인해주세요")
         }
-        alert("아디이와 비밀번호를 확인해주세요")
+        // alert("아디이와 비밀번호를 확인해주세요")  //-->  try , catch 로 에러 핸들링 해서 백엔드에서 받아온 데이터로 알림창 띄우기에 이젠 필요 없다
     }
+    //----------------------------------------------------------------------------------------------
 
 
     return <S.Form onSubmit={handleSubmit(handlePressSignIn)}>
